ef2ad7c6d267202b6b3fda4f88e7b29b5b093efd
Christian Edensor Arbon <ceva1116@gmail.com>
2013-05-31 11:55:28 +0900

    katta form for adjective

---
+++ katta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QuickSharp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Input a japstring");
+            string input = Console.ReadLine();
+            Console.WriteLine(katta(input));
+        }
+        
+        public static string chopper1(string meat, string knife){
+            int marker = meat.Length - knife.Length;
+            string ret = "";
+            for(int cnt = 0; cnt < marker; cnt++){
+                ret = ret + meat[cnt];
+            }
+            return ret;
+        }
+        
+        private static string katta(string input){
+            if(input.EndsWith("かった")){
+                return chopper1(input, "かった") + "い";
+            }
+            return "";
+        }
+    }
+}
